d6c27aeddcfb31ccdc711f42eee32dc5c8550714
savoieric <savoieric@hotmail.com>
2016-04-07 23:51:16 -0400

    Tweaked constants

--- constants.lua
+++ constants.lua
@@ -1,14 +1,14 @@
-PULL_LENGTH2 = 300 * 300; -- Square of how far player can pull
-PULL_FORCE = 400; -- Force at which the player pulls the ball
-PLAYER_FORCE = 5000; -- Force put on the player to move the joystick
+PULL_LENGTH2 = 325 * 325; -- Square of how far player can pull
+PULL_FORCE = 500; -- Force at which the player pulls the ball
+PLAYER_FORCE = 6000; -- Force put on the player to move the joystick
 PLAYER_DAMPENING = 5; -- Dampening on player movement
 PLAYER_DENSITY = 10; -- Player density (higher means more mass)
 PLAYER_HITPOINTS = 100;
-BALL_DAMAGE = 10;
+BALL_DAMAGE = 8;
 
 BALL_RADIUS = 8;
-BALL_DAMAGE_SPEED_SCALING = 500; -- Higher leads to less damage based on speed
-BALL_MAX_DAMAGE = 20;
+BALL_DAMAGE_SPEED_SCALING = 450; -- Higher leads to less damage based on speed
+BALL_MAX_DAMAGE = 25;
 
 PLAYER_RADIUS = 16; -- Size of the player's circle
 PLAYER_INVULNERABILITY_DURATION = 1; -- Duration of invulnerability following a player's hit (s)
