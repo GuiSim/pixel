04112365467f0cd13375d3aef505738da157d189
gsimard <gsimard@coveo.com>
2016-04-08 15:41:08 -0400

    Added reset and quit button

--- state/menu.lua
+++ state/menu.lua
@@ -5,7 +5,8 @@ local menu = {
   selections = {
     {playerCount = 2},
     {playerCount = 4}
-  }
+  },
+  escapeHeldFor = 0
 }
 
 
@@ -16,12 +17,31 @@ local menuTwoPlayers = love.graphics.newImage("assets/textures/startscreen_2play
 local menuFourPlayers = love.graphics.newImage("assets/textures/startscreen_4player.png")
 local cursor = love.graphics.newImage("assets/textures/startscreen_arrow.png")
 
+function menu:update(dt)
+  -- Hold escape to quit
+  if love.keyboard.isDown("escape") or love.joystick.getJoysticks()[1]:isGamepadDown("guide") then 
+    menu.escapeHeldFor = menu.escapeHeldFor + dt
+  else 
+    menu.escapeHeldFor = 0
+  end
+  
+  if menu.escapeHeldFor > 1 then
+    love.event.quit()
+  end
+  
+end
+
+
 function menu:draw()
   love.graphics.draw(menuBackground, 0, 0);
   love.graphics.draw(menuTitle, 0, 0);
   love.graphics.draw(menuTwoPlayers, 0, 0);
   love.graphics.draw(menuFourPlayers,0 ,0);
   love.graphics.draw(cursor, 650, menu.selection * 200 + 350)
+  
+  if (menu.escapeHeldFor > 0.1) then
+    love.graphics.print("Hold escape or 'Guide' to quit!", 0, 0, 0, 3, 3)
+  end
 end
 
 function menu:joystickpressed(joystick, button)
